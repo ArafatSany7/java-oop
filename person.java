2185b5e621c88ee71a6c8459c006133cf23453a3
Arafat Sany <human.sany7@gmail.com>
2025-02-28 00:00:03 +0600

    first commit

---
+++ person.java
@@ -0,0 +1,16 @@
+
+public class person {
+
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        person person1 = new person();
+
+        person1.name = "sany";
+        person1.age = 21;
+
+        System.out.println("name of the person is " + person1.name + " age of the person is : " + person1.age);
+
+    }
+}
